36d2afede5ef54304b6de2b03f50fa567f995fcc
Ted Barbushev <teddb@ymail.com>
2017-10-09 21:59:37 -0600

    Fixed a bug where a command was being processed twice.
    Added ability to send longer commands and the device would process
    4 characters at a time by assuming the 4th character to be a new line.
    
    Cleaned up the code.

--- ARC_FW.ino
+++ ARC_FW.ino
@@ -3,22 +3,22 @@
 const char FIRMWARE_VERSION[] = "1000";  // will start at 1000 and go up
 const uint32_t BAUD_RATE = 9600;
 const char FAULT_RESP[] = "FLT";
-
 const uint8_t PIN_RANGE_MIN = 2;   //pins numbers between range_min and range_max will be used
 const uint8_t PIN_RANGE_MAX = 19;
-
 
 char inputBuffer[4];   //incoming commands will be 3 chars at most + null
 uint8_t inBufPos = 0;
 bool commandComplete = false;
 
 // function prototypes
+void processCommand(void);
+uint8_t getPinNumber(void);
+bool pinNumberIsValid(const uint8_t pinNum);
 void clearInputBuffer(void);
 
-
 void setup() {	
-    //set watchdog to 500ms
-    wdt_enable(WDTO_500MS);  
+  //set watchdog to 500ms
+  wdt_enable(WDTO_500MS);  
     
 	//set all pins to output
 	for (uint8_t pin = PIN_RANGE_MIN; pin <= PIN_RANGE_MAX; pin++ )
@@ -30,57 +30,32 @@ void setup() {
 
 
 void loop() {
-	
 	wdt_reset();
 
 	while (Serial.available())
 	{
 		// get character from serial port
 		char inChar = (char)Serial.read();
+    inputBuffer[inBufPos++] = inChar;
 
-		// set complete flag is command is done
-		if (inChar == '\n')
+		// set complete flag if command is done or if inputBuffer is full
+		if (inChar == '\n' || inBufPos >= sizeof(inputBuffer))
 		{
-			inputBuffer[inBufPos] = '\0'; //add null terminator
+			inputBuffer[inBufPos - 1] = '\0'; //add null terminator
 			commandComplete = true;
-			break;
-		}
-		else
-			inputBuffer[inBufPos++] = inChar;
-
-		if (inBufPos >= sizeof(inputBuffer))
-		{
-			clearInputBuffer();
-			Serial.println(FAULT_RESP);
 			break;
 		}
 	}
 
-
 	if (commandComplete)
 	{
 		processCommand();
-		uint8_t pin = 2;
-
-
-		switch (inputBuffer[0])
-		{
-			case 'H': digitalWrite(pin, HIGH);
-				break;
-			case 'L': digitalWrite(pin, LOW);
-				break;
-
-			default:
-				Serial.println(FAULT_RESP);
-		}
-
-
 		clearInputBuffer();
 	}
 }
 
 
-void processCommand()
+void processCommand(void)
 {
 	uint8_t pin = getPinNumber();
 
@@ -107,13 +82,13 @@ void processCommand()
 }
 
 
-uint8_t getPinNumber()
+uint8_t getPinNumber(void)
 {
 	return atoi(&inputBuffer[1]);
 }
 
 
-bool pinNumberIsValid(uint8_t pinNum)
+bool pinNumberIsValid(const uint8_t pinNum)
 {		
 	return ((pinNum >= PIN_RANGE_MIN) && (pinNum <= PIN_RANGE_MAX));
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ARC_FW
+
+Arduino Relay Control
+
+Controls pin states based on commands received via the USB serial interface.
+Serial operates at 9600 baud 8N1.
+
+Command syntax:
+S#/n, where (S)tate is either (H)igh or (L)ow, and # is pin #, /n represents a new line and is used for end of transmission.
+
+Valid pin numbers are 2 to 19. Pins 0 and 1 are reserved for USB serial communication.
+If the command is accepted and executed, it will be echoed back.
+In case of an invalid command, the device will return "FLT".
+
+Example "L2/n" will set pin 2 Low, and "H2/n" will set pin 2 high.
+
+Note: 
+	The command buffer is 4 characters long. Commands longer than 4 characters and not containing a new line will be processed 4 characters by
+	overwriting the 4th character as a new line.
